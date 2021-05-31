ed9d0cd72a57cbbf51e11a66f03cc6a6cbeb9f83
Nicolas <nico_0104@live.fr>
2021-05-31 14:15:55 +0200

    fin de correction du service + servlet pour le add avec new DTO + new mapper

--- src/main/java/com/excilys/cdb/servlet/AddComputer.java
+++ src/main/java/com/excilys/cdb/servlet/AddComputer.java
@@ -5,34 +5,50 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.excilys.cdb.binding.dto.DtoCompanyServletService;
+import com.excilys.cdb.binding.dto.DtoComputerServletService;
+import com.excilys.cdb.binding.mapper.MapperDtoComputerServletService;
+import com.excilys.cdb.binding.validation.ValidationDtoCompany;
+import com.excilys.cdb.binding.validation.ValidationDtoComputer;
 import com.excilys.cdb.model.Company;
 import com.excilys.cdb.service.Service;
-import java.io.* ;
-import java.util.* ;
+import java.io.*;
+import java.util.*;
 
+@WebServlet("/AddComputer")
+public class AddComputer extends HttpServlet {
+	Service service = Service.getInstance();
 
-@WebServlet ("/AddComputer")
-public class AddComputer  extends HttpServlet {
-		Service service =Service.getInstance();
-
-	 public  void doGet(HttpServletRequest request, HttpServletResponse response)
-	 throws ServletException, IOException  {
-		 List<Company> listCompany = service.getListCompany();
-		 request.setAttribute("listCompany",listCompany);
-		this.getServletContext().getRequestDispatcher( "/WEB-INF/jsp/addComputer.jsp" ).forward( request, response );
-		
-
-		
+	private void addComputer(String name, String introduced, String discontinued, String company_id) {
+		try {	
+			DtoCompanyServletService dtoCompany = new DtoCompanyServletService(Integer.parseInt(company_id));
+			if (ValidationDtoCompany.getInstance().isValidDto(dtoCompany)) {
+				DtoComputerServletService dtoComputer = new DtoComputerServletService(name, introduced, discontinued,
+						dtoCompany);
+				if (ValidationDtoComputer.getInstance().isValidDto(dtoComputer)) {
+					
+					service.addComputer(MapperDtoComputerServletService.dtoToComputer(dtoComputer));
+				}
+			}
+		}catch(NumberFormatException e) {
+			e.getStackTrace();
+		}
 	}
 
-	 public  void doPost(HttpServletRequest request, HttpServletResponse response)
-	 throws ServletException, IOException  {
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		List<Company> listCompany = service.getListCompany();
+		request.setAttribute("listCompany", listCompany);
+		this.getServletContext().getRequestDispatcher("/WEB-INF/jsp/addComputer.jsp").forward(request, response);
+
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String name = request.getParameter("name");
 		String introduced = request.getParameter("introduced");
 		String discontinued = request.getParameter("discontinued");
 		String company = request.getParameter("company");
-		service.addComputer(name,introduced,discontinued,company);
-		
-		doGet(request, response) ;
+		this.addComputer(name, introduced, discontinued, company);
+		doGet(request, response);
 	}
-}	
+}
